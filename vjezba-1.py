c650f120832ee90425d7cfe96775622901c3eca5
Hriberski <114573061+Hriberski@users.noreply.github.com>
2022-09-28 11:24:12 +0200

    Add files via upload

---
+++ vjezba-1.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+print("Učimo python!")
+ime = input("Kako se zoveš?")
+print ("Pozdrav, " + ime)
+x=8
+print(x)
+pi=3.14
+print(pi)
+poruka= "Ovo su znakovi - Stringovi"
+print(poruka)
